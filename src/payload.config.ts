b7d363d90e367600f20a9ea57b08b225dc99eecc
Bhungane Nkululeko Mthimkulu <55452673+shadow5og@users.noreply.github.com>
2024-10-12 20:57:31 +0200

    Added the google gscStorage plugin and configured it for google buckets uploads. Also updated .gitignore

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,13 +1,23 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { gcsStorage } from '@payloadcms/storage-gcs'
+import { JWT } from 'google-auth-library'
+import keys from 'key.json'
 import path from 'path'
 import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { fileURLToPath } from 'url'
 import { CampApplications } from './app/collections/CampApplications'
 import { ProofOfPayment } from './app/collections/ProofOfPayment'
 import Users from './app/collections/Users'
+
+const authClient = new JWT({
+  email: keys.client_email,
+  keyFile: process.env.PAYLOAD_GCS_ADC_FILE_NAME,
+  key: keys.private_key,
+  scopes: [process.env.PAYLOAD_GCS_SCOPES as string],
+})
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -26,12 +36,23 @@ export default buildConfig({
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   db: postgresAdapter({
+    push: false,
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
   }),
   sharp,
   plugins: [
-    // storage-adapter-placeholder
+    gcsStorage({
+      collections: {
+        proofOfPayment: true,
+      },
+      bucket: process.env.PAYLOAD_GCS_BUCKET as string,
+      options: {
+        projectId: process.env.PAYLOAD_GCS_PROJECT_ID,
+        authClient,
+      },
+      acl: 'Public',
+    }),
   ],
 })
