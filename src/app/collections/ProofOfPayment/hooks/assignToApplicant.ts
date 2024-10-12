7adad2819d68c349211152ef4d308349a9394dbf
Bhungane Nkululeko Mthimkulu <55452673+shadow5og@users.noreply.github.com>
2024-10-12 21:15:10 +0200

    Modified assignToApplicant to tag files instead of saving them in folders

--- src/app/collections/ProofOfPayment/hooks/assignToApplicant.ts
+++ src/app/collections/ProofOfPayment/hooks/assignToApplicant.ts
@@ -1,40 +1,38 @@
-import { ProofOfPayment } from "@/payload-types";
-import { CollectionBeforeChangeHook } from "payload";
-import { slugify } from "../../../utilities/string";
+import { ProofOfPayment } from '@/payload-types'
+import { CollectionBeforeChangeHook } from 'payload'
+import { slugify } from '../../../utilities/string'
 
-export const assignToApplicant: CollectionBeforeChangeHook<
-  ProofOfPayment
-> = async ({
+export const assignToApplicant: CollectionBeforeChangeHook<ProofOfPayment> = async ({
   data, // incoming data to update or create with
   req: { user, payload }, // full express request
   operation, // name of the operation ie. 'create', 'update'
 }) => {
-  const newData = { ...data };
-  let changed = false;
+  const newData = { ...data }
+  let changed = false
 
-  if (operation === "create" && !data.filename?.includes("/")) {
+  if (operation === 'create' && !data.filename?.includes('/')) {
     const { fullName } = await payload.findByID({
-      collection: "users",
+      collection: 'users',
       id: data.user as number,
       depth: 0,
       user,
-    });
+    })
 
-    const sluggifiedName = slugify(fullName);
+    const sluggifiedName =
+      slugify(fullName) + '-' + (typeof data.user === 'number' ? data.user : data.user?.id)
 
     await payload.delete({
-      collection: "proofOfPayment",
+      collection: 'proofOfPayment',
       where: {
         filename: { contains: sluggifiedName },
       },
       depth: 0,
-    });
+    })
 
-    newData.filename = `${sluggifiedName}-${typeof data.user === 'number' ? data.user : data.user?.id}/${newData.filename}`;
-    changed = true;
+    newData.filename = `${sluggifiedName}-${newData.filename}`
+    newData.url = process.env.PAYLOAD_GCS_BUCKET_URL + '/' + newData.filename
+    changed = true
   }
 
-  console.log(newData);
-
-  return changed ? newData : data; // Return data to either create or update a document with
-};
+  return changed ? newData : data // Return data to either create or update a document with
+}
